641910184a9ff469cec6fc228e13f39bb4690992
kince <katherine.ince0390@gmail.com>
2021-04-19 03:57:08 -0500

    details

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import PropType from 'prop-types'
+import './Result/Result.css'
 
-const Result = ({propValue}) => (
-    <div className="result">
-        {propValue}
+const Result = ({propValue, propHistory}) => (
+    <div>
+        <div className="result">
+            <div className="history">
+                {propHistory}
+            </div>
+            <div className="total">
+                {propValue}
+            </div>
+        </div>
     </div>
 )
 
 //Valida Argumentos con la libreria prop-types
 Result.propType = {
-    propValue: PropType.string.isRequired
+    propValue: PropType.string.isRequired,
+    propHistory: PropType.string
 }
 Result.defaultProps =  {
     propValue: "0"
